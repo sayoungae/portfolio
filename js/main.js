e8fcf45c79fe508e9601245c5619f00dabbe25dc
sayoungae <duddo0305@gmail.com>
2025-05-13 14:05:11 +0900

    c

--- js/main.js
+++ js/main.js
@@ -1,43 +1,43 @@
 
 $(document).ready(function(){
-    $('#fullpage').fullpage({
-        licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
-        navigation: true,
-        navigationPosition: 'right',
-        autoScrolling:true,
-        // scrollHorizontally: true,
-        keyboardScrolling: true,
-		slidesNavigation: true,
-		slidesNavPosition: 'bottom',
-		anchors: ['intro', 'secondPage', '3rdPage', '4thpage', '5thpage','6thpage', 'lastpage'],
-        onLeave: function(){
-			$('.section [data-aos]').each(function(){
-				$(this).removeClass("aos-animate");
-			});
-		},
-		onSlideLeave: function(){
-			$('.slide [data-aos]').each(function(){
-				$(this).removeClass("aos-animate")
-			});
-		},
-		afterSlideLoad: function(){
-			$('.slide.active [data-aos]').each(function(){
-				$(this).addClass("aos-animate")
-			});
-		},
-		afterLoad: function(){
-			$('.section.active [data-aos]').each(function(){
-				$(this).addClass("aos-animate")
-			});
-			let section = $('.section');
-			section.removeClass('on')
-			if(section.hasClass('active')){
-				this.addClass('on')
-			}else{
-				this.removeClass('on')
-			}
-		}
-    });
+    // $('#fullpage').fullpage({
+    //     licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
+    //     navigation: true,
+    //     navigationPosition: 'right',
+    //     autoScrolling:true,
+    //     // scrollHorizontally: true,
+    //     keyboardScrolling: true,
+	// 	slidesNavigation: true,
+	// 	slidesNavPosition: 'bottom',
+	// 	anchors: ['intro', 'secondPage', '3rdPage', '4thpage', '5thpage','6thpage', 'lastpage'],
+    //     onLeave: function(){
+	// 		$('.section [data-aos]').each(function(){
+	// 			$(this).removeClass("aos-animate");
+	// 		});
+	// 	},
+	// 	onSlideLeave: function(){
+	// 		$('.slide [data-aos]').each(function(){
+	// 			$(this).removeClass("aos-animate")
+	// 		});
+	// 	},
+	// 	afterSlideLoad: function(){
+	// 		$('.slide.active [data-aos]').each(function(){
+	// 			$(this).addClass("aos-animate")
+	// 		});
+	// 	},
+	// 	afterLoad: function(){
+	// 		$('.section.active [data-aos]').each(function(){
+	// 			$(this).addClass("aos-animate")
+	// 		});
+	// 		let section = $('.section');
+	// 		section.removeClass('on')
+	// 		if(section.hasClass('active')){
+	// 			this.addClass('on')
+	// 		}else{
+	// 			this.removeClass('on')
+	// 		}
+	// 	}
+    // });
 	
 	const elts = {
 		text1: document.getElementById("txt1"),
